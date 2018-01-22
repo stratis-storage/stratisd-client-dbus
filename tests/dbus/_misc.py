c1c8f0b7cf13a5d1c661462802a27a06cc74ea46
Tony Asleson <tasleson@redhat.com>
2018-01-22 16:26:58 -0600

    Add loop back create/delete functionality
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- tests/dbus/_misc.py
+++ tests/dbus/_misc.py
@@ -22,6 +22,9 @@ import string
 import subprocess
 
 
+import tempfile
+import uuid
+
 from hypothesis import strategies
 
 _STRATISD = os.environ['STRATISD']
@@ -75,10 +78,127 @@ class ServiceR(ServiceABC):
 
     def setUp(self):
         if MODE == SIMULATOR and not self._not_sim:
-            self._stratisd = subprocess.Popen([os.path.join(_STRATISD), '--sim'])
+            self._stratisd = subprocess.Popen([os.path.join(_STRATISD),
+                                               '--sim'])
         else:
             self._stratisd = subprocess.Popen([os.path.join(_STRATISD),
                                                '--debug'])
 
 
 Service = ServiceR
+
+
+BIN = os.getenv('STRATIS_LOSETUP_BIN', "/usr/sbin/losetup")
+
+
+class LoopBackDevices(object):
+    """
+    Class for creating and managing loop back devices which are needed for
+    specific types of udev event testing.
+    """
+
+    def __init__(self):
+        """
+        Class constructor which creates a temporary directory to store backing
+        file in.
+        """
+        self.dir = tempfile.mkdtemp('_stratis_loop_back')
+        self.count = 0
+        self.devices = {}
+
+    def create_device(self, size_mib):
+        """
+        Create a new loop back device, sparse backing file and attaching it.
+
+        Note: The first time a loop back device is known it will generate
+        a udev "add" event, subsequent backing file changes do not, thus we
+        will need to generate it synthetically.
+        :param size_mib:
+        :return: opaque handle, done as device representing block device will
+                 change.
+        """
+        backing_file = self.dir + '/block_device_%d' % self.count
+        self.count += 1
+
+        with open(backing_file, 'ab') as bd:
+            bd.truncate(size_mib * (1024 * 1024))
+
+        result = subprocess.run([BIN, '-f', '--show', backing_file],
+                                check=True, stdout=subprocess.PIPE)
+        device = str.strip(result.stdout.decode("utf-8"))
+        token = uuid.uuid4()
+        self.devices[token] = (device, backing_file)
+        return token
+
+    def unplug(self, token):
+        """
+        Remove the device from the /dev tree, but doesn't remove backing file
+        :param token: Opaque representation of some loop back device
+        :return: None
+        """
+        if token in self.devices:
+            (device, backing_file) = self.devices[token]
+            subprocess.run([BIN, '-d', device], check=True)
+            self.devices[token] = (None, backing_file)
+
+    def generate_udev_add_event(self, token):
+        """
+        Synthetically create "add" udev event for this loop back device
+        :param token: Opaque representation of some loop back device
+        :return: None
+        """
+        if token in self.devices:
+            (device, _) = self.devices[token]
+
+            if device:
+                device_name = device.split('/')
+                with open("/sys/block/%s/uevent" % device_name[-1], "w") as e:
+                    e.write("add")
+
+    def hotplug(self, token):
+        """
+        Attaches an existing backing file to a loop back device
+        :param token: Opaque representation of some loop back device
+        :return:
+        """
+        if token in self.devices:
+            (_, backing_file) = self.devices[token]
+
+            result = subprocess.run([BIN, '-f', '--show', backing_file],
+                                    check=True, stdout=subprocess.PIPE)
+            device = str.strip(result.stdout.decode("utf-8"))
+            self.devices[token] = (device, backing_file)
+
+            # Make sure an add occurs
+            self.generate_udev_add_event(token)
+
+    def device_file(self, token):
+        """
+        Return the device block device full name for a loopback token
+        :param token: Opaque representation of some loop back device
+        :return: Full file path or None if not currently attached
+        """
+        if token in self.devices:
+            return self.devices[token][0]
+
+    def destroy_devices(self):
+        """
+        Detach loopbacks and remove backing files
+        :return:
+        """
+        for (device, backing_file) in self.devices.values():
+            if device is not None:
+                subprocess.run([BIN, '-d', device], check=True)
+            os.remove(backing_file)
+
+        self.devices = {}
+        self.count = 0
+
+    def destroy_all(self):
+        """
+        Detach all the devices and delete the file(s) and directory!
+        :return: None
+        """
+        self.destroy_devices()
+        os.rmdir(self.dir)
+        self.dir = None
